e863a8bf759779e1178c9dd6205b3aea7e8c9c11
森考喜 <morikouki@morikoukinoMacBook-Pro.local>
2020-07-24 15:06:59 +0900

    [Add]5-9課題

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1ccd85fda8596c56f7ace1159fb100f0115eb4889987752fba9770d4bb6cc25fed82bc56dd74b8cca10cb5afb15be5e52d418f1bb62c3294b4bf45477d4cd9fd'
