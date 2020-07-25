f6be2ed1bcdb487bcf8293370ec01f3c17578178
森考喜 <morikouki@morikoukinoMacBook-Pro.local>
2020-07-25 19:20:43 +0900

    [Add]5-9課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path
+    redirect_to book_path(@book.id)
   end
 
   def destroy
