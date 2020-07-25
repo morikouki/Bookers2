6b422be7031b3a0d6885c8ce537781114973736e
森考喜 <morikouki@morikoukinoMacBook-Pro.local>
2020-07-25 22:02:36 +0900

    [Add]5-9課題

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,11 @@ class UsersController < ApplicationController
 
   def index
   	@users = User.all
-    
   end
 
   def show
   	@user = User.find(params[:id])
+    @book = Book.new
     @books = @user.books
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   
   devise_for :users
-  resources :users
+  resources :users 
   resources :books
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
